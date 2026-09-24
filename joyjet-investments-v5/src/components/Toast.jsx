4efc27a470315bdb43d4872389a9d490923ee98e
diana shawn <indiavvs767@gmail.com>
2026-09-24 01:59:31 +0100

    Delete joyjet-investments-v5 directory

--- joyjet-investments-v5/src/components/Toast.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Toast({ message, type = 'info' }) {
-  return (
-    <div className={`toast ${type}`} role="status" aria-live="polite">
-      {message}
-    </div>
-  )
-}
